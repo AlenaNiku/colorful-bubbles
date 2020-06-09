b117872e1bac4a7b5861ee06bf85b4728d26e315
Alena Nikulina <alenanikulina0@gmail.com>
2020-06-09 15:39:45 -0400

    added an event listener that triggers the addBubbles function

---
+++ main.js
@@ -0,0 +1,21 @@
+// function that adds and removes the bubbles
+addBubbles = function(e) {
+    let body = document.querySelector('body')
+    let circle = document.createElement('span')
+    let x = e.offsetX
+    let y = e.offsetY
+    circle.style.left = x + "px"
+    circle.style.top = y + "px"
+    let size = Math.random() * 100
+    circle.style.width = 20 + size + "px"
+    circle.style.height = 20 + size + "px"
+
+    body.appendChild(circle)
+
+    setTimeout(function() {
+        circle.remove()
+    }, 1800)
+}
+
+// add an event listener to the whole document on mousemove
+document.addEventListener('mousemove', addBubbles)
